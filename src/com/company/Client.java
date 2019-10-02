5fbcb23f56aacb4703866cb37675c60f944bf5e0
Anders Pedersen <anderspk@hotmail.com>
2019-10-02 15:07:57 +0200

    ..

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -2,13 +2,46 @@ package com.company;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
 
-    private String username;
+    private static boolean forbundet = true;
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        System.out.println("Please choose a username:");
+    while (forbundet == true) {
+        BufferedReader userInput =
+                new BufferedReader(new InputStreamReader(System.in));
+        String input = userInput.readLine();
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        byte[] sendLeave = new byte[1024];
+        if (input.equalsIgnoreCase("Exit")){
+            System.out.println("Leaving server");
+            String leaveSentence = "Client: " + input + ", " + IPAddress + ":" + "9876" + " has left the server";
+            sendLeave = leaveSentence.getBytes();
+            DatagramPacket closePacket = new DatagramPacket(sendLeave, sendLeave.length, IPAddress, 9876);
+            clientSocket.send(closePacket);
+            forbundet = false;
+            clientSocket.close();
+        }
+        else {
+            String sentence = "JOIN " + input + ", " + IPAddress + ":" + "9876";
+            sendData = sentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String modifiedSentence = new String(receivePacket.getData());
+            System.out.println("FROM SERVER: " + modifiedSentence);
+             }
+
+        }
+
     }
+
 
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -2,11 +2,41 @@ package com.company;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Server {
 
-    public static void main(String[] args) {
-        // write your code here
+    public static void main(String[] args) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        List<ArrayList> client = new ArrayList<>();
+
+        while(true)
+        {
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String( receivePacket.getData());
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String parse = sentence;
+            String[] splitParse = parse.trim().split("\\s+");
+            System.out.println(Arrays.toString(splitParse));
+            
+            client.add(splitParse[1]);
+            String capitalizedSentence = sentence.toUpperCase();
+            System.out.println("RECEIVED: " + sentence);
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket =
+                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+
+    public void checkUsername(){
+
     }
 
 }
