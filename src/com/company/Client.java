14b798f0d292e9d95e2e5ddec94b1f0e3b4de1dd
Anders Pedersen <anderspk@hotmail.com>
2019-10-02 12:32:07 +0200

    Initial commit

---
+++ src/com/company/Client.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+    private String username;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+}
