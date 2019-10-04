8291d10db2703c49295f1d5542156969ba00d546
Kundby <anderspk@hotmail.com>
2019-10-05 00:29:37 +0200

    ..

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -2,42 +2,81 @@ package com.company;
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 public class Client {
 
-    private static boolean forbundet = true;
+    private boolean forbundet = true;
+    private boolean username_Chosen = false;
+    private String username;
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Please choose a username:");
-    while (forbundet == true) {
-        BufferedReader userInput =
-                new BufferedReader(new InputStreamReader(System.in));
-        String input = userInput.readLine();
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
-        byte[] sendLeave = new byte[1024];
-        if (input.equalsIgnoreCase("Exit")){
-            System.out.println("Leaving server");
-            String leaveSentence = "Client: " + input + ", " + IPAddress + ":" + "9876" + " has left the server";
-            sendLeave = leaveSentence.getBytes();
-            DatagramPacket closePacket = new DatagramPacket(sendLeave, sendLeave.length, IPAddress, 9876);
-            clientSocket.send(closePacket);
-            forbundet = false;
-            clientSocket.close();
-        }
-        else {
-            String sentence = "JOIN " + input + ", " + IPAddress + ":" + "9876";
-            sendData = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            clientSocket.send(sendPacket);
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
-            String modifiedSentence = new String(receivePacket.getData());
-            System.out.println("FROM SERVER: " + modifiedSentence);
-             }
+        Client client = new Client(9876);
+        client.executeClient();
+    }
+
+    public Client(int port){
+
+    }
+
+    private void executeClient() {
+        while (forbundet == true) {
+            try{
+
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+            byte[] sendLeave = new byte[1024];
+                //If no username is chosen, or already taken
+                while(username_Chosen == false){
+                    System.out.println("Please choose a username:");
+                    BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                    username = userInput.readLine();
+                    String sentence = "JOIN, " + username + " - " + IPAddress + ":" + "9876";
+                    sendData = sentence.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+                    clientSocket.send(sendPacket);
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    clientSocket.receive(receivePacket);
+                    String modifiedSentence = new String(receivePacket.getData());
+                    if (modifiedSentence.contains("ERROR: Username is already taken")){
+                        System.out.println(modifiedSentence);
+                        username_Chosen = false;
+                    }//If you wrote a username that isn't taken
+                    else{
+                        System.out.println("FROM SERVER: " + modifiedSentence);
+                        username_Chosen = true;
+                    }
+
+                }
+             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+             String input = userInput.readLine();
+             //Used to leave server and close socket
+            if (input.equalsIgnoreCase("Exit")){
+                System.out.println("Leaving server");
+                String leaveSentence = "Client: " + username + ", " + IPAddress + ":" + "9876" + " has left the server";
+                sendLeave = leaveSentence.getBytes();
+                DatagramPacket closePacket = new DatagramPacket(sendLeave, sendLeave.length, IPAddress, 9876);
+                clientSocket.send(closePacket);
+                forbundet = false;
+                clientSocket.close();
+                //Used to broadcast a msg
+            }else if (input.toLowerCase().contains("Broadcast".toLowerCase())){
+                String sentence = " " + input + ", " +  "by user: " + username + " " + IPAddress + ":" + "9876";
+                sendData = sentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+                clientSocket.send(sendPacket);
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
+                String modifiedSentence = new String(receivePacket.getData());
+                System.out.println("FROM SERVER: " + modifiedSentence);
+            }
+
+        }catch (UnknownHostException e){
+                System.out.println("UnknownHostException: " + e);
+            }catch (IOException e){
+                System.out.println("IOException: " + e);
+            }
 
         }
 
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -7,10 +7,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Server {
+    static List<String> clientList = new ArrayList<>();
+    static private boolean usernameError = false;
 
     public static void main(String[] args) throws Exception {
         DatagramSocket serverSocket = new DatagramSocket(9876);
-        List<ArrayList> client = new ArrayList<>();
+
 
         while(true)
         {
@@ -18,25 +20,72 @@ public class Server {
             byte[] sendData = new byte[1024];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            String sentence = new String( receivePacket.getData());
+            String sentence = new String(receivePacket.getData());
             InetAddress IPAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
-            String parse = sentence;
-            String[] splitParse = parse.trim().split("\\s+");
-            System.out.println(Arrays.toString(splitParse));
-            
-            client.add(splitParse[1]);
-            String capitalizedSentence = sentence.toUpperCase();
+
             System.out.println("RECEIVED: " + sentence);
-            sendData = capitalizedSentence.getBytes();
-            DatagramPacket sendPacket =
-                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
-            serverSocket.send(sendPacket);
+                //Splits join msg into 2 Strings, one containing username and IPadress + port, and adds that to arraylist of active clients
+                if (sentence.contains("JOIN")){
+                    String parse = sentence;
+                    String[] splitParse = parse.trim().split(",");
+                    String parseUsername = splitParse[1].toLowerCase();
+                    String[] parseFurther = parseUsername.trim().split("-");
+                    String parsedFully = parseFurther[0];
+                    //Loops through clientList to check if wanted username, already exists
+                    for (int i = 0; i < clientList.size(); i++){
+                        String tempName = clientList.get(i).toLowerCase();
+                        if (tempName.contains(parsedFully)){
+                            String capitalizedSentence = "ERROR: Username is already taken, Please try again!";
+                            sendData = capitalizedSentence.getBytes();
+                            DatagramPacket sendPacket =
+                                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                            serverSocket.send(sendPacket);
+                            System.out.println("User tried to connect with username already in use: " + parsedFully);
+                            usernameError = true;
+                        }else{
+                            usernameError = false;
+                        }
+                    }
+                    //If username doesnt exist, add to list and reply with success msg
+                    if (usernameError == false) {
+                        clientList.add(parseUsername);
+                        System.out.println("Current users: " + clientList);
+                        String capitalizedSentence = "Connection accepted! " + sentence.toUpperCase();
+                        sendData = capitalizedSentence.getBytes();
+                        DatagramPacket sendPacket =
+                                new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                        serverSocket.send(sendPacket);
+                    }
+                }
+                //When Exit msg is received, then client is removed from the arraylist of active clients
+                else if (sentence.toLowerCase().contains("has left the server".toLowerCase())){
+                    String parse = sentence;
+                    String[] splitParse = parse.trim().split(":");
+                    String parseUsername = splitParse[1].toLowerCase();
+                    String[] parseFurther = parseUsername.trim().split(",");
+                    String parsedFully = parseFurther[0];
+                    System.out.println(Arrays.toString(splitParse));
+                    for (int i = 0; i < clientList.size(); i++){
+                        String tempName = clientList.get(i).toLowerCase();
+                        if (tempName.contains(parsedFully)){
+                            String removed = clientList.remove(i);
+                            System.out.println("Client left: " + removed);
+                        }
+                    }
+                    System.out.println("Current users: " + clientList);
+                    //Used to broadcast a msg, from one client to all clients
+                }else if (sentence.toLowerCase().contains("Broadcast".toLowerCase())){
+                    System.out.println("Broadcast test");
+                    for (int i = 0; i < clientList.size(); i++){
+                        String capitalizedSentence = "Broadcast: " + sentence.toUpperCase();
+                        sendData = capitalizedSentence.getBytes();
+                        DatagramPacket broadcastPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                        serverSocket.send(broadcastPacket);
+                    }
+                }
+
         }
-    }
-
-    public void checkUsername(){
-
     }
 
 }
